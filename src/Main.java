6ba7cc24e740dfaa720d7e01227bf282da0bc047
Jack <youngdragon0@gmail.com>
2023-02-12 14:03:01 +0300

    added out to git ignore

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         int amount = 4_500; // стоимость билета
-      int miles_costs = 20; // стоимость одной бонусной мили
+        int miles_costs = 20; // стоимость одной бонусной мили
         int bonus = amount / miles_costs; //колличество полученных бонусов
 
-        System.out.println("Итоговый бонус: " + bonus );
+        System.out.println("Итоговый бонус: " + bonus);
 
     }
 }
