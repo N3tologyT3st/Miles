6f2254422b4aeb3bc702d924ad4e4b8a40baee83
Jack <youngdragon0@gmail.com>
2023-02-12 13:54:20 +0300

    add miles costs

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         int amount = 4_500; // стоимость билета
-      int x = 20; // стоимость одной бонусной мили
-        int bonus = amount / x; //колличество полученных бонусов
+      int miles_costs = 20; // стоимость одной бонусной мили
+        int bonus = amount / miles_costs; //колличество полученных бонусов
 
         System.out.println("Итоговый бонус: " + bonus );
 
