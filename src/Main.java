88161af377b6ef873ea2ac9612ce5918b9a39b9f
Jack <youngdragon0@gmail.com>
2023-02-12 16:17:02 +0300

    added clas BonusMilesService

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        int amount = 4_500; // стоимость билета
-        int miles_costs = 20; // стоимость одной бонусной мили
-        int bonus = amount / miles_costs; //колличество полученных бонусов
-
-        System.out.println("Итоговый бонус: " + bonus);
-
+        BonusMilesService service = new BonusMilesService();
+        int price = 4_500;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
+
